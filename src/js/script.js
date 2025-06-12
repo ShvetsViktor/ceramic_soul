bb886896bf57ad4ce935dc54f04cccde9c4472d0
Viktor <shvetsviktor89@gmail.com>
2025-06-12 13:03:03 +0100

    integrating slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,41 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 import "/src/sass/style.scss";
+
+
+try {
+    const swiper = new Swiper('.works__slider', {
+        slidesPerView: 1,
+        modules: [Navigation, Pagination],
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 5
+            },
+            1920: {
+                spaceBetween: 35
+            }
+        },
+        // Optional parameters
+        loop: true
+
+    });
+} catch { }
+
+// const slider = document.querySelector('.swiper').swiper;
